31b86e47fe19bc0e8d55c18ebf6116c51d983048
Adilkhan Altynbek <adilkhanaltynbek@MBPA.local>
2025-03-11 09:42:18 +0500

    1

--- Bash-Scripts/Bash-Git-Add.sh
+++ Bash-Scripts/Bash-Git-Add.sh
@@ -2,5 +2,13 @@ echo " "; echo "GIT | remote"; git remote -v
 echo " "; echo "GIT | branch"; git branch
 echo " "; echo "GIT | add";    git add .
 echo " "; echo "GIT | commit"; git commit -m "1"
+
+echo " "; echo "GIT | pull"; git pull --rebase origin main
+if [ $? -ne 0 ]; then
+    echo "Конфликты при pull. Завершение ребейза..."
+    git add .
+    git rebase --continue
+fi
+
 echo " "; echo "GIT | push";   git push origin main
 echo " "; echo "GIT | rebase"; git pull --rebase origin main
