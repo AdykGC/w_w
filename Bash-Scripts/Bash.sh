d79b85039cdb56587a1467e6e0cc5e5017fea5fe
Adilkhan Altynbek <adilkhanaltynbek@MBPA.local>
2025-03-11 09:28:48 +0500

    1

--- Bash-Scripts/Bash.sh
+++ Bash-Scripts/Bash.sh
@@ -23,6 +23,18 @@ if [ $? -eq 0 ]; then echo "script1.sh выполнен успешно."
 else echo "script1.sh завершился с ошибкой."; exit 1; fi
 
 echo "\n\n ------------------------------------------------------------------------------------------------"
+echo "Выполнение git pull для синхронизации с удалённым репозиторием..."
+git pull origin main --rebase
+if [ $? -eq 0 ]; then echo "git pull выполнен успешно."
+else echo "git pull завершился с ошибкой."; exit 1; fi
+
+echo "\n\n ------------------------------------------------------------------------------------------------"
 echo "Запуск Bash-Git-Disconnect.sh..."; ./Bash-Scripts/Bash-Git-Disconnect.sh
 if [ $? -eq 0 ]; then echo "script1.sh выполнен успешно."
 else echo "script1.sh завершился с ошибкой."; exit 1; fi
+
+echo "\n\n ------------------------------------------------------------------------------------------------"
+echo "Выполнение git push для отправки изменений в удалённый репозиторий..."
+git push origin main
+if [ $? -eq 0 ]; then echo "git push выполнен успешно."
+else echo "git push завершился с ошибкой."; exit 1; fi
