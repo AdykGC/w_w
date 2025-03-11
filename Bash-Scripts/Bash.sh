27ac519d3af5cb276c32755e0f757bc5ecf7d063
Adilkhan Altynbek <adilkhanaltynbek@MBPA.local>
2025-03-11 09:15:02 +0500

    1

--- Bash-Scripts/Bash.sh
+++ Bash-Scripts/Bash.sh
@@ -11,3 +11,16 @@ parent_dir=$(dirname "$current_dir")
 
 echo "$current_dir";
 echo "$parent_dir";
+
+
+echo "Запуск Bash-Git-Connect.sh..."; ./Bash-Scripts/Bash-Git-Connect.sh
+if [ $? -eq 0 ]; then echo "script1.sh выполнен успешно."
+else echo "script1.sh завершился с ошибкой."; exit 1; fi
+
+echo "Запуск Bash-Git-Add.sh..."; ./Bash-Scripts/Bash-Git-Add.sh
+if [ $? -eq 0 ]; then echo "script1.sh выполнен успешно."
+else echo "script1.sh завершился с ошибкой."; exit 1; fi
+
+echo "Запуск Bash-Git-Disconnect.sh..."; ./Bash-Scripts/Bash-Git-Disconnect.sh
+if [ $? -eq 0 ]; then echo "script1.sh выполнен успешно."
+else echo "script1.sh завершился с ошибкой."; exit 1; fi
