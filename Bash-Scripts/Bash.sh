748fabc8d18519b49c6c80a8d89a3f64411b948b
Adilkhan Altynbek <adilkhanaltynbek@MBPA.local>
2025-03-11 09:46:54 +0500

    1

--- Bash-Scripts/Bash.sh
+++ Bash-Scripts/Bash.sh
@@ -12,35 +12,19 @@ parent_dir=$(dirname "$current_dir")
 echo "$current_dir";
 echo "$parent_dir";
 
-echo "\n\n ------------------------------------------------------------------------------------------------"
+
+echo "\n\n"; echo "------------------------------------------------------------------------------------------------"
 echo "Запуск Bash-Git-Connect.sh..."; ./Bash-Scripts/Bash-Git-Connect.sh
-if [ $? -eq 0 ]; then echo "script1.sh выполнен успешно."
-else echo "script1.sh завершился с ошибкой."; exit 1; fi
+if [ $? -eq 0 ]; then echo "Bash-Git-Connect.sh выполнен успешно."
+else echo "Bash-Git-Connect.sh завершился с ошибкой."; exit 1; fi
 
-echo "\n\n ------------------------------------------------------------------------------------------------"
+echo "\n\n"; echo "------------------------------------------------------------------------------------------------"
 echo "Запуск Bash-Git-Add.sh..."; ./Bash-Scripts/Bash-Git-Add.sh
-if [ $? -eq 0 ]; then echo "script1.sh выполнен успешно."
-else echo "script1.sh завершился с ошибкой."; exit 1; fi
+if [ $? -eq 0 ]; then echo "Bash-Git-Add.sh выполнен успешно."
+else echo "Bash-Git-Add.sh завершился с ошибкой."; exit 1; fi
 
-echo "\n\n ------------------------------------------------------------------------------------------------"
-<<<<<<< HEAD
-echo "Выполнение git pull для синхронизации с удалённым репозиторием..."
-git pull origin main --rebase
-if [ $? -eq 0 ]; then echo "git pull выполнен успешно."
-else echo "git pull завершился с ошибкой."; exit 1; fi
-
-echo "\n\n ------------------------------------------------------------------------------------------------"
+echo "\n\n"; echo "------------------------------------------------------------------------------------------------"
 echo "Запуск Bash-Git-Disconnect.sh..."; ./Bash-Scripts/Bash-Git-Disconnect.sh
-if [ $? -eq 0 ]; then echo "script1.sh выполнен успешно."
-else echo "script1.sh завершился с ошибкой."; exit 1; fi
+if [ $? -eq 0 ]; then echo "Bash-Git-Disconnect.sh выполнен успешно."
+else echo "Bash-Git-Disconnect.sh завершился с ошибкой."; exit 1; fi
 
-echo "\n\n ------------------------------------------------------------------------------------------------"
-echo "Выполнение git push для отправки изменений в удалённый репозиторий..."
-git push origin main
-if [ $? -eq 0 ]; then echo "git push выполнен успешно."
-else echo "git push завершился с ошибкой."; exit 1; fi
-=======
-echo "Запуск Bash-Git-Disconnect.sh..."; ./Bash-Scripts/Bash-Git-Disconnect.sh
-if [ $? -eq 0 ]; then echo "script1.sh выполнен успешно."
-else echo "script1.sh завершился с ошибкой."; exit 1; fi
->>>>>>> a228290 (1)
